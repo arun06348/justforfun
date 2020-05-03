4c16aee376bac99e080581890a04089ef79f96f3
arun06348 <50420018+arun06348@users.noreply.github.com>
2020-05-03 09:37:34 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,10 @@
+function clickLove()
+{
+    alert("thank you so much for loving me!");
+    document.write("love you to");
+}
+function clickHate()
+{
+    alert("its okay");
+    document.write("every one has their own life to decide what they want");
+}
